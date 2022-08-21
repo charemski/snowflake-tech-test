f8137bfb500c46ce96bcf72da130fea917cb81eb
charemski <93057347+charemski@users.noreply.github.com>
2022-08-22 00:41:10 +0200

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,89 @@
+########################
+# terraform main file to create bucket and load python generated files
+########################
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+
+  required_version = ">= 0.15"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+
+########################
+# Bucket creation
+########################
+resource "aws_s3_bucket" "my_protected_bucket" {
+  bucket = var.bucket_name
+}
+
+##########################
+# Bucket private access
+##########################
+resource "aws_s3_bucket_acl" "my_protected_bucket_acl" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  acl    = "private"
+}
+
+#############################
+# Enable bucket versioning
+#############################
+resource "aws_s3_bucket_versioning" "my_protected_bucket_versioning" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+
+##########################################
+# Enable default Server Side Encryption
+##########################################
+resource "aws_s3_bucket_server_side_encryption_configuration" "my_protected_bucket_server_side_encryption" {
+  bucket = aws_s3_bucket.my_protected_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+    }
+  }
+}
+
+
+
+########################
+# Disabling bucket
+# public access
+########################
+resource "aws_s3_bucket_public_access_block" "my_protected_bucket_access" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+
+  # Block public access
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+########################
+# Loading multiple files
+# into bucket
+########################
+resource "aws_s3_bucket_object" "object1" {
+for_each = fileset("input_data/", "*")
+bucket = aws_s3_bucket.my_protected_bucket.id
+key = each.value
+source = "input_data/${each.value}"
+etag = filemd5("input_data/${each.value}")
+}
